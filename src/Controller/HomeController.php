e36dc9c3c28781af6162a8ce90597594e54068d3
audreyfcz <fcz.audrey@gmail.com>
2022-09-17 16:49:48 +0200

    display home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,18 @@
 
 namespace App\Controller;
 
+use App\Repository\TrickRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(): Response
+    #[Route('/', name: 'index')]
+    public function index(TrickRepository $trickRepository): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'tricks' => $trickRepository->findAll()
         ]);
     }
 }
